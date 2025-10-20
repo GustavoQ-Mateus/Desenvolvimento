1d5c97ab5231e1776f96724d890c5a1b67ba4c4a
GustavoQ-Mateus <gustavoq.mateusgithub@gmail.com>
2025-10-20 18:06:43 -0300

    Update app

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", function(req,res){
-    res.send("<h1>Bem vindo ao !</h1>");
+    res.send("<h1>Bem vindo ao Meu site!</h1>");
 });
 
 app.get("/produtos", function(req,res){
